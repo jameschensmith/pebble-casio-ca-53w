1c6d10e2051f5eb617bb45e392d582254d138ed4
James Smith <poeia@users.noreply.github.com>
2016-11-09 12:39:40 -0500

    Version 1.1 - Display Cleanup.
    Imported from Cloud Pebble. Time, period, day, and date displays have all been cleaned up by replacing fonts with gBitmaps. Menu icon has also been fixed with transparency. Also, there was a bluetooth connection bug where it vibrated every time the watchface appeared and bluetooth was disconnected. Hopefully this has been fixed. Will continue to test.

--- src/c/casio.c
+++ src/c/casio.c
@@ -8,37 +8,75 @@
 
 static Window *windowMain;
 
+static bool appStarted = false;
+
 static GBitmap     *bgBitmap;
 static GBitmap     *btConIconBitmap, *btDisIconBitmap;
 static GBitmap     *batFullIconBitmap, *batChargingIconBitmap, *batEmptyIconBitmap, *bat33IconBitmap, *bat66IconBitmap;
 static BitmapLayer *bgLayer, *btIconLayer, *batIconLayer;
 
-static TextLayer *layerTime;   // "12:34 56"
-static TextLayer *layerDate;   // "YY MM-DD"
-static TextLayer *layerDay;    // "SU" "MO" "TU" "WE" "TH" "FR" "SA"
-static TextLayer *layerPeriod; // "AM" "PM"
+static GBitmap     *imageDay;
+static BitmapLayer *layerDay;
+const int IMAGE_DAY_RESOURCE_IDS[] = {
+  RESOURCE_ID_IMAGE_DAY_SU,
+  RESOURCE_ID_IMAGE_DAY_MO,
+  RESOURCE_ID_IMAGE_DAY_TU,
+  RESOURCE_ID_IMAGE_DAY_WE,
+  RESOURCE_ID_IMAGE_DAY_TH,
+  RESOURCE_ID_IMAGE_DAY_FR,
+  RESOURCE_ID_IMAGE_DAY_SA
+};
 
-static GFont fontTime;
-static GFont fontDay;
-static GFont fontPeriod;
+#define TOTAL_DIGITS 8
+static GBitmap     *imagesTimeDigits[TOTAL_DIGITS], *imagesDateDigits[TOTAL_DIGITS];
+static BitmapLayer *layersTimeDigits[TOTAL_DIGITS], *layersDateDigits[TOTAL_DIGITS];
+const int IMAGE_NUM_RESOURCE_IDS[] = {
+  RESOURCE_ID_IMAGE_NUM_0,
+  RESOURCE_ID_IMAGE_NUM_1,
+  RESOURCE_ID_IMAGE_NUM_2,
+  RESOURCE_ID_IMAGE_NUM_3,
+  RESOURCE_ID_IMAGE_NUM_4,
+  RESOURCE_ID_IMAGE_NUM_5,
+  RESOURCE_ID_IMAGE_NUM_6,
+  RESOURCE_ID_IMAGE_NUM_7,
+  RESOURCE_ID_IMAGE_NUM_8,
+  RESOURCE_ID_IMAGE_NUM_9
+};
 
+static GBitmap     *imagePeriod;
+static BitmapLayer *layerPeriod;
+
+static void set_container_image(GBitmap **bmp_image, BitmapLayer *bmp_layer, const int resource_id, GPoint origin);
 static void main_window_load(Window *window);
 static void main_window_unload(Window *window);
-static void update_time(void);
-static void handle_timer(struct tm *tick_time, TimeUnits units_changed);
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed);
 static void handle_accel(AccelAxisType axis, int32_t direction);
 static void callback_bluetooth(bool connected);
 static void callback_battery(BatteryChargeState state);
+
+static void set_container_image(GBitmap **bmp_image, BitmapLayer *bmp_layer, const int resource_id, GPoint origin) {
+  //Temp variable for old image
+  GBitmap *old_image = *bmp_image;
+
+  //Replace old image with new image
+  *bmp_image = gbitmap_create_with_resource(resource_id);
+  GRect frame = (GRect) {
+    .origin = origin,
+    .size = gbitmap_get_bounds(*bmp_image).size
+  };
+  bitmap_layer_set_bitmap(bmp_layer, *bmp_image);
+  layer_set_frame(bitmap_layer_get_layer(bmp_layer), frame);
+
+  //Get rid of the old image if it exits
+  if (old_image != NULL) {
+    gbitmap_destroy(old_image);
+  }
+}
 
 static void main_window_load(Window *window) {
   // Get the bounds of the Window
   Layer *windowLayer = window_get_root_layer(window);
   GRect windowBounds = layer_get_bounds(windowLayer);
-  
-  // Font
-  fontTime = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_28));
-  fontDay = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_20));
-  fontPeriod = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DROID_10));
   
   // Background
   bgBitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
@@ -65,42 +103,34 @@ static void main_window_load(Window *window) {
   bitmap_layer_set_compositing_mode(batIconLayer, GCompOpSet);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(batIconLayer));
   
-  // Time Layer
-  layerTime = text_layer_create(GRect(0, 35, windowBounds.size.w, 50));
-  text_layer_set_font(layerTime, fontTime);
-  text_layer_set_text_alignment(layerTime, GTextAlignmentCenter);
-  text_layer_set_background_color(layerTime, GColorClear);
-  text_layer_set_text_color(layerTime, GColorBlack);
-  text_layer_set_text(layerTime, "00:00 00"); // Must hold 8 characters
-  layer_add_child(windowLayer, text_layer_get_layer(layerTime));
+  // Time Layer, "12:34 56"
+  memset(&layersTimeDigits, 0, sizeof(layersTimeDigits));
+  memset(&imagesTimeDigits, 0, sizeof(imagesTimeDigits));
+  for (int i = 0; i < TOTAL_DIGITS; i++) {
+    layersTimeDigits[i] = bitmap_layer_create(windowBounds);
+    bitmap_layer_set_compositing_mode(layersTimeDigits[i], GCompOpSet);
+    layer_add_child(windowLayer, bitmap_layer_get_layer(layersTimeDigits[i]));
+  }
   
-  // Date Layer, Shake to show
-  layerDate = text_layer_create(GRect(0, 35, windowBounds.size.w, 50));
-  text_layer_set_font(layerDate, fontTime);
-  text_layer_set_text_alignment(layerDate, GTextAlignmentCenter);
-  text_layer_set_background_color(layerDate, GColorClear);
-  text_layer_set_text_color(layerDate, GColorBlack);
-  text_layer_set_text(layerDate, "YY MM-DD"); // Must hold 8 characters
-  layer_add_child(windowLayer, text_layer_get_layer(layerDate));
-  layer_set_hidden(text_layer_get_layer(layerDate), true);
+  // Date Layer, "YY MM-DD", shake to show
+  memset(&layersDateDigits, 0, sizeof(layersDateDigits));
+  memset(&imagesDateDigits, 0, sizeof(imagesDateDigits));
+  for (int i = 0; i < TOTAL_DIGITS; i++) {
+    layersDateDigits[i] = bitmap_layer_create(windowBounds);
+    bitmap_layer_set_compositing_mode(layersDateDigits[i], GCompOpSet);
+    layer_add_child(windowLayer, bitmap_layer_get_layer(layersDateDigits[i]));
+    layer_set_hidden(bitmap_layer_get_layer(layersDateDigits[i]), true);
+  }
   
-  // Day Layer
-  layerDay = text_layer_create(GRect(0, 20, windowBounds.size.w - 24, 25));
-  text_layer_set_font(layerDay, fontDay);
-  text_layer_set_text_alignment(layerDay, GTextAlignmentRight);
-  text_layer_set_background_color(layerDay, GColorClear);
-  text_layer_set_text_color(layerDay, GColorBlack);
-  text_layer_set_text(layerDay, "SU");
-  layer_add_child(windowLayer, text_layer_get_layer(layerDay));
+  // Day Layer, "SU" "MO" "TU" "WE" "TH" "FR" "SA"
+  layerDay = bitmap_layer_create(windowBounds);
+  bitmap_layer_set_compositing_mode(layerDay, GCompOpSet);
+  layer_add_child(windowLayer, bitmap_layer_get_layer(layerDay));
   
-  // Period Layer
-  layerPeriod = text_layer_create(GRect(21, 32, windowBounds.size.w - 21, 25));
-  text_layer_set_font(layerPeriod, fontPeriod);
-  text_layer_set_text_alignment(layerPeriod, GTextAlignmentLeft);
-  text_layer_set_background_color(layerPeriod, GColorClear);
-  text_layer_set_text_color(layerPeriod, GColorBlack);
-  text_layer_set_text(layerPeriod, "AMPM");
-  layer_add_child(windowLayer, text_layer_get_layer(layerPeriod));
+  // Period Layer, "AM" "PM"
+  layerPeriod = bitmap_layer_create(windowBounds);
+  bitmap_layer_set_compositing_mode(layerPeriod, GCompOpSet);
+  layer_add_child(windowLayer, bitmap_layer_get_layer(layerPeriod));
   
   // Show the correct state of the BT connection from the start
   callback_bluetooth(connection_service_peek_pebble_app_connection());
@@ -108,11 +138,30 @@ static void main_window_load(Window *window) {
 
 // Free allocated data.
 static void main_window_unload(Window *window) {
-  // Destroy Text Layers
-  text_layer_destroy(layerPeriod);
-  text_layer_destroy(layerDay);
-  text_layer_destroy(layerDate);
-  text_layer_destroy(layerTime);
+  // Period Layer
+  layer_remove_from_parent(bitmap_layer_get_layer(layerPeriod));
+  bitmap_layer_destroy(layerPeriod);
+  gbitmap_destroy(imagePeriod);
+  
+  // Day Layer
+  layer_remove_from_parent(bitmap_layer_get_layer(layerDay));
+  bitmap_layer_destroy(layerDay);
+  gbitmap_destroy(imageDay);
+  
+  // Date Layer
+  for (int i = 0; i < TOTAL_DIGITS; i++) {
+    layer_remove_from_parent(bitmap_layer_get_layer(layersDateDigits[i]));
+    gbitmap_destroy(imagesDateDigits[i]);
+    bitmap_layer_destroy(layersDateDigits[i]);
+  }
+  
+  // Time Layer
+  for (int i = 0; i < TOTAL_DIGITS; i++) {
+    layer_remove_from_parent(bitmap_layer_get_layer(layersTimeDigits[i]));
+    gbitmap_destroy(imagesTimeDigits[i]);
+    bitmap_layer_destroy(layersTimeDigits[i]);
+  }
+  
   // Destroy Battery Icon
   gbitmap_destroy(batFullIconBitmap);
   gbitmap_destroy(batChargingIconBitmap);
@@ -120,85 +169,93 @@ static void main_window_unload(Window *window) {
   gbitmap_destroy(bat33IconBitmap);
   gbitmap_destroy(bat66IconBitmap);
   bitmap_layer_destroy(batIconLayer);
+  
   // Destroy Bluetooth Icon
   gbitmap_destroy(btConIconBitmap);
   gbitmap_destroy(btDisIconBitmap);
   bitmap_layer_destroy(btIconLayer);
+  
   // Destroy Background
   bitmap_layer_destroy(bgLayer);
   gbitmap_destroy(bgBitmap);
-  // Destroy Custom Font
-  fonts_unload_custom_font(fontPeriod);
-  fonts_unload_custom_font(fontDay);
-  fonts_unload_custom_font(fontTime);
 }
 
-static void update_time(void) {
-  // Get a tm structure
-  time_t temp = time(NULL);
-  struct tm *tickTime = localtime(&temp);
-
-  // Set the current hour, minute, and second
-  static char bufferTime[9];
-  strftime(bufferTime, sizeof(bufferTime), clock_is_24h_style() ?
-                                          "%H:%M %S" : "%I:%M %S", tickTime);
-  text_layer_set_text(layerTime, bufferTime);
-  
-  // Set the Date
-  static char bufferDate[9];
-  strftime(bufferDate, sizeof(bufferDate), "%y %m-%d", tickTime);
-  text_layer_set_text(layerDate, bufferDate);
-  
-  // Set the current day
-  static char bufferDay[3];
-  switch(tickTime->tm_wday) {
-    case 0:
-      strncpy(bufferDay, "SU", sizeof(bufferDay));
-      break;
-    case 1:
-      strncpy(bufferDay, "MO", sizeof(bufferDay));
-      break;
-    case 2:
-      strncpy(bufferDay, "TU", sizeof(bufferDay));
-      break;
-    case 3:
-      strncpy(bufferDay, "WE", sizeof(bufferDay));
-      break;
-    case 4:
-      strncpy(bufferDay, "TH", sizeof(bufferDay));
-      break;
-    case 5:
-      strncpy(bufferDay, "FR", sizeof(bufferDay));
-      break;
-    case 6:
-      strncpy(bufferDay, "SA", sizeof(bufferDay));
-      break;
-    default:
-      break;
+static unsigned short get_display_hour(unsigned short hour) {
+  if (clock_is_24h_style()) {
+    return hour;
   }
-  text_layer_set_text(layerDay, bufferDay);
+  unsigned short display_hour = hour % 12;
+
+  // Converts "0" to "12"
+  return display_hour ? display_hour : 12;
+}
+
+static void update_days(struct tm *tick_time) {
+  // Set the date with the format "YY MM-DD"
+  set_container_image(&imagesDateDigits[0], layersDateDigits[0], IMAGE_NUM_RESOURCE_IDS[(tick_time->tm_year/10)%10], GPoint(21, 45));
+  set_container_image(&imagesDateDigits[1], layersDateDigits[1], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_year%10], GPoint(34, 45));
+  set_container_image(&imagesDateDigits[3], layersDateDigits[3], IMAGE_NUM_RESOURCE_IDS[(tick_time->tm_mon+1)/10], GPoint(60, 45));
+  set_container_image(&imagesDateDigits[4], layersDateDigits[4], IMAGE_NUM_RESOURCE_IDS[(tick_time->tm_mon+1)%10], GPoint(73, 45));
+  set_container_image(&imagesDateDigits[5], layersDateDigits[5], RESOURCE_ID_IMAGE_NUM_DASH, GPoint(86, 45));
+  set_container_image(&imagesDateDigits[6], layersDateDigits[6], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_mday/10], GPoint(99, 45));
+  set_container_image(&imagesDateDigits[7], layersDateDigits[7], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_mday%10], GPoint(112, 45));
   
-  // Set the current period
-  static char bufferPeriod[5];
-  if(tickTime->tm_hour < 12) {
-    strncpy(bufferPeriod, "AM  ", sizeof(bufferPeriod));
+  // Set the day
+  set_container_image(&imageDay, layerDay, IMAGE_DAY_RESOURCE_IDS[tick_time->tm_wday], GPoint(99, 27));
+}
+
+static void update_hours(struct tm *tick_time) {
+  // Set the hours
+  unsigned short displayHour = get_display_hour(tick_time->tm_hour);
+  set_container_image(&imagesTimeDigits[0], layersTimeDigits[0], IMAGE_NUM_RESOURCE_IDS[displayHour/10], GPoint(21, 45));
+  set_container_image(&imagesTimeDigits[1], layersTimeDigits[1], IMAGE_NUM_RESOURCE_IDS[displayHour%10], GPoint(34, 45));
+  set_container_image(&imagesTimeDigits[2], layersTimeDigits[2], RESOURCE_ID_IMAGE_NUM_COLON, GPoint(47, 45));
+  
+  // Set the period
+  if(tick_time->tm_hour < 12) {
+    set_container_image(&imagePeriod, layerPeriod, RESOURCE_ID_IMAGE_PERIOD_AM, GPoint(21, 36));
   } else {
-    strncpy(bufferPeriod, "  PM", sizeof(bufferPeriod));
+    set_container_image(&imagePeriod, layerPeriod, RESOURCE_ID_IMAGE_PERIOD_PM, GPoint(32, 36));
   }
-  text_layer_set_text(layerPeriod, bufferPeriod);
 }
 
-static void handle_timer(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+static void update_minutes(struct tm *tick_time) {
+  set_container_image(&imagesTimeDigits[3], layersTimeDigits[3], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_min/10], GPoint(60, 45));
+  set_container_image(&imagesTimeDigits[4], layersTimeDigits[4], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_min%10], GPoint(73, 45));
+}
+
+static void update_seconds(struct tm *tick_time) {
+  set_container_image(&imagesTimeDigits[6], layersTimeDigits[6], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_sec/10], GPoint(99, 45));
+  set_container_image(&imagesTimeDigits[7], layersTimeDigits[7], IMAGE_NUM_RESOURCE_IDS[tick_time->tm_sec%10], GPoint(112, 45));
+}
+
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
+  if (units_changed & DAY_UNIT) {
+    update_days(tick_time);
+  }
+  if (units_changed & HOUR_UNIT) {
+    update_hours(tick_time);
+  }
+  if (units_changed & MINUTE_UNIT) {
+    update_minutes(tick_time);
+  }
+  if (units_changed & SECOND_UNIT) {
+    update_seconds(tick_time);
+  }
 }
 
 static void handle_switch() {
-  if(layer_get_hidden(text_layer_get_layer(layerTime))) {
-    layer_set_hidden(text_layer_get_layer(layerTime), false);
-    layer_set_hidden(text_layer_get_layer(layerDate), true);
+  // Switch to time layer if it is currently hidden.
+  if(layer_get_hidden(bitmap_layer_get_layer(layersTimeDigits[0]))) {
+    for (int i = 0; i < TOTAL_DIGITS; i++) {
+      layer_set_hidden(bitmap_layer_get_layer(layersTimeDigits[i]), false);
+      layer_set_hidden(bitmap_layer_get_layer(layersDateDigits[i]), true);
+    }
   } else {
-    layer_set_hidden(text_layer_get_layer(layerTime), true);
-    layer_set_hidden(text_layer_get_layer(layerDate), false);
+    for (int i = 0; i < TOTAL_DIGITS; i++) {
+      layer_set_hidden(bitmap_layer_get_layer(layersTimeDigits[i]), true);
+      layer_set_hidden(bitmap_layer_get_layer(layersDateDigits[i]), false);
+    }
   }
 }
 
@@ -212,7 +269,9 @@ static void callback_bluetooth(bool connected) {
   if(!connected) {
     bitmap_layer_set_bitmap(btIconLayer, btDisIconBitmap);
     // Issue a vibrating alert
-    vibes_double_pulse();
+    if (appStarted) {
+      vibes_double_pulse();
+    }
   } else {
     bitmap_layer_set_bitmap(btIconLayer, btConIconBitmap);
   }
@@ -246,11 +305,14 @@ void init(void) {
   });
   window_stack_push(windowMain, true);
   
-  update_time();
+  // Avoids a blank screen on watch start.
+  time_t now = time(NULL);
+  struct tm *tick_time = localtime(&now);  
+  handle_tick(tick_time, DAY_UNIT + HOUR_UNIT + MINUTE_UNIT + SECOND_UNIT);
   // Ensure battery level is displayed from the start
   callback_battery(battery_state_service_peek());
 
-  tick_timer_service_subscribe(SECOND_UNIT, handle_timer);
+  tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
   accel_tap_service_subscribe(handle_accel);
   
   // Register for Bluetooth connection updates
@@ -260,6 +322,8 @@ void init(void) {
   
   // Register for battery level updates
   battery_state_service_subscribe(callback_battery);
+  
+  appStarted = true;
 }
 
 void deinit(void) {
